80d1ac9078c0da8996c84d3f2966f2dc24aac3f4
J Mulder <jlmulder2014@gmail.com>
2020-06-04 21:17:38 -0700

    user input setup

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // Assignment code here
 
 
+
+function generatePassword() {
+  var passwordLength = 0
+  
+  //ask user for password length
+         //if # is not 8-128 will continue to prompt
+      while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        passwordLength = parseInt(prompt("How long do you want the length of the password? Enter a number 8 - 128 to continue"));
+      }
+
+      console.log(passwordLength); 
+      window.alert("Valid Character Length");
+          
+  var lowercase = confirm("Would you like to include lowercase letters?");
+  var uppercaser = confirm("Would you like to include uppercase letters?");
+  var numeric = confirm("Would you like to include numbers?");
+  var specialcharacters = confirm("Would you like to include special characters?");
+  
+  
+  }
+
+  
+    
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
